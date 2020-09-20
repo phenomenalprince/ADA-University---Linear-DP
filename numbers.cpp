5249ead846a170774d5a865c44369437c8f4a1f9
phenomenalprince <54848663+phenomenalprince@users.noreply.github.com>
2020-09-20 18:31:55 +0530

    Create numbers.cpp

---
+++ numbers.cpp
@@ -0,0 +1,27 @@
+/*
+    “Don't raise your voice, improve your argument."
+                                      - Desmond Tutu
+*/
+
+        #include <bits/stdc++.h>
+        using namespace std;
+        
+        #define ll long long int
+        #define sonic               ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+        
+        int main()
+        {
+            int n;
+            cin >> n;
+            int dp[n+1];
+            memset(dp,0,sizeof(dp));
+            dp[0]=0,dp[1]=2,dp[2]=4;
+            for (int i=3;i<=n;i++)
+            {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            cout << dp[n] << endl;
+            
+            return 0;
+        }
+
